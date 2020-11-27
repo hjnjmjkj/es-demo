9aa80bbcbc36b0e4ec93289a9043e551c547f811
huangkai02 <huangkai02@sunlands.com>
2020-11-27 15:00:07 +0800

    spring data api

--- src/main/java/com/hk/es/repository/ItemRepository.java
+++ src/main/java/com/hk/es/repository/ItemRepository.java
@@ -1,7 +1,10 @@
 package com.hk.es.repository;
 
 import com.hk.es.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ItemRepository  extends ElasticsearchRepository<Item,Long> {
+    Page<Item> findByTitleOrCategoryOrBrand(String keyword, String keyword1, String keyword2, Pageable pageable);
 }
